3b84fc65403f62cec7bfd7dc5feba8c6c15f453d
imaakarsh <aakarsh027@outlook.com>
2026-07-19 20:16:39 +0530

    refactor: comment out unused variable definitions in Variable-DataTypes.cpp

--- Basic-C++/Variable-Datatypes/Variable-DataTypes.cpp
+++ Basic-C++/Variable-Datatypes/Variable-DataTypes.cpp
@@ -3,23 +3,34 @@ using namespace std;
 
 int main()
 {
-    // variable define
-    int marks = 100;
-    cout << "My Marks is:" << marks;
+    // // variable define
+    // int marks = 100;
+    // cout << "My Marks is:" << marks;
 
-    // declarition
-    int age;
+    // // declarition
+    // int age;
 
-    // defination
-    int age = 21;
-    cout << age;
+    // // defination
+    // int age = 21;
+    // cout << age;
 
-    // manipulation or updation
-    age = 23;
-    
+    // // manipulation or updation
+    // age = 23;
 
+    // int
+    int count = 5;
+    // float
+    float share = 3.455;
+    // char
+    char alpha = 'z';
+    // double
+    double w = 55.69887;
+    // boolean
+    bool isMale = true;
 
-
-
+    cout << count << endl ;
+    cout << share << endl ;
+    cout << alpha << endl ;
+    cout << w << endl ;
     return 0;
 }
