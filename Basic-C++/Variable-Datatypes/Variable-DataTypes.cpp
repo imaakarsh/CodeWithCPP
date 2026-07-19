af3430cc4e8668140cc8d613152a47f222a386ec
imaakarsh <aakarsh027@outlook.com>
2026-07-19 19:50:21 +0530

    feat: add build task configuration and update Variable-DataTypes.cpp output

--- Basic-C++/Variable-Datatypes/Variable-DataTypes.cpp
+++ Basic-C++/Variable-Datatypes/Variable-DataTypes.cpp
@@ -3,8 +3,23 @@ using namespace std;
 
 int main()
 {
+    // variable define
     int marks = 100;
-    cout << marks;
+    cout << "My Marks is:" << marks;
+
+    // declarition
+    int age;
+
+    // defination
+    int age = 21;
+    cout << age;
+
+    // manipulation or updation
+    age = 23;
+    
+
+
+
 
     return 0;
 }
