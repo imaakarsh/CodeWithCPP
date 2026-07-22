ae00b20afeec44e6912084d640fc232bb85d70b2
imaakarsh <aakarsh027@outlook.com>
2026-07-23 01:39:14 +0530

    feat: implement do-while loop example and update continue statement logic

--- Loops/continue.cpp
+++ Loops/continue.cpp
@@ -2,17 +2,29 @@
 using namespace std;
 int main()
 {
-    int n, i;
-    // cout << "enter the number: ";
-    // cin >> n;
-    for (i = 1; i <= 5; i++){
+    // int n, i;
+    // // C
+    // for (i = 1; i <= 5; i++){
+    //     if (i == 4){
+    //         continue;
+    //     }
+
+    // cout << i << endl;
+    // }
     
-        if (i == 4){
+
+    // while loop 
+    int i=1 ,n;
+    cout << "Enter The N:" << endl;
+    cin >> n ;
+    while(i<=n){
+        if(i==5){
+            i++;
             continue;
         }
-
-    cout << i << endl;
+        cout << i ;
+        i = i+1;
     }
-    
+
     return 0;
 }
---
+++ Loops/dowhile.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i =1;
+    do
+    {
+        cout << i << "";
+        i = i+1;
+    } while (i<=5);
+    cout << endl;
+    
+
+
+
+
+
+
+    return 0;
+}
