b28c96852315da753e8a3b6b565cd7d93baec8b8
imaakarsh <aakarsh027@outlook.com>
2026-07-23 22:59:00 +0530

    refactor: comment out unused conditional logic in main2.cpp

--- Operators/main2.cpp
+++ Operators/main2.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
-    bool cond1 = true;
-    bool cond2 = false;
-    bool cond3 = false;
+    // bool cond1 = true;
+    // bool cond2 = false;
+    // bool cond3 = false;
 
-    if(cond1 || cond2 || cond3){
-        cout << "all are true" << endl;
-    }
-    else{
-        cout << "not true" << endl;
-    }
+    // if(cond1 || cond2 || cond3){
+    //     cout << "all are true" << endl;
+    // }
+    // else{
+    //     cout << "not true" << endl;
+    // }
+    int arr = 5;
+    arr+= 10;
+    cout << arr;
     return 0;
 }
