e8d3e90e152049070ee0fd4039fe9f14075f6874
imaakarsh <aakarsh027@outlook.com>
2026-07-29 21:32:27 +0530

    Add 21.cpp and main.cpp with initial implementations

---
+++ 2D Array/21.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char arr[10];
+
+    char arr[10] = "Aakarsh";
+    cout << arr << endl;
+    return 0;
+}
---
+++ String/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // declare
+    string name = "aakarsh";
+
+    cout << "Length:" << name.length() << endl;
+
+    //  initializ
+    // string name1 = "aakarsh";
+
+    // name.push_back('Aakarsh');
+    // name.push_back('Raj');
+    // cout << name << endl;
+
+    return 0;
+}
