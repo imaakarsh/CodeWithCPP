fcaacf883cc7e5d8fb4adfdd94af1f6bce949a83
imaakarsh <aakarsh027@outlook.com>
2026-08-01 21:19:41 +0530

    Add class abc with getter and setter methods; update main function

--- OPPS/const.cpp
+++ OPPS/const.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 using namespace std;
+class abc
+{
+    int x;
+    int *y;
+
+public:
+    abc()
+    {
+        x = 0;
+        y = new int(0);
+    }
+
+    int getX(){
+        return x;
+    }
+    void setX(int _val){
+        x = _val;
+    }
+    int getY(){
+        return *y;
+    }
+
+    void setY(int _val){
+        *y = _val;
+    }
+};
 
 int main()
+{
+    abc a;
+    cout << a.getX() << endl;
+    return 0;
+}
+
+int main2()
 {
     // const int x = 5; // x is const
     // init can be done;
@@ -19,15 +52,15 @@ int main()
     // cout << *a << endl;
 
     // CONST pointer , but non constant data.
-    int *const a = new int(2);
-    *a = 20;
-    cout << *a;
-    int b = 50;
-    // a = &b; -> nhi chlega
+    // int *const a = new int(2);
+    // *a = 20;
+    // cout << *a;
+    // int b = 50;
+    // // a = &b; -> nhi chlega
 
-    // const varaible const data;
-    const int *const a = new int(10);
-    cout << *a << endl;
+    // // const varaible const data;
+    // const int *const a = new int(10);
+    // cout << *a << endl;
 
-    return 0;
+    // return 0;
 }
