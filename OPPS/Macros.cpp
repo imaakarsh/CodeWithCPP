b70f9cb177dda8a9c89b3e6958f5c3f1aa671431
imaakarsh <aakarsh027@outlook.com>
2026-08-01 21:49:03 +0530

    Add Macros.cpp with circleArea function and main implementation; include Macros.exe binary

---
+++ OPPS/Macros.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define PI 3.14
+#define MAXX(x, y) (x > y ? x : y)
+
+float circleArea(int r)
+{
+    return PI * r * r;
+}
+
+void fun(){
+    int 
+}
+int main()
+{
+
+    float area = circleArea(3);
+    cout << area << endl;
+
+    return 0;
+}
