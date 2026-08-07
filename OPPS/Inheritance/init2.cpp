f7db87522ff778cb2dc5d9edbda3698a5f508633
imaakarsh <aakarsh027@outlook.com>
2026-08-07 08:29:01 +0530

    fix: correct class name from 'Vehicals' to 'Vehicles' and improve constructor logging

--- OPPS/Inheritance/init2.cpp
+++ OPPS/Inheritance/init2.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
 
-class Vehicals
+class Vehicles
 {
-public:
+protected:
     string name;
+
+public:
     string modle;
     int noOfTyres;
 
-    Vehicals(string _name, string _modle, int _noOfTyres)
+    string getName()
     {
-        cout << "Im Inside Vehicals ctor" << endl;
+        return this->name;
+    }
+
+    Vehicles(string _name, string _modle, int _noOfTyres)
+    {
+        cout << "Im Inside Vehicles ctor" << endl;
         this->name = _name;
         this->modle = _modle;
         this->noOfTyres = _noOfTyres;
@@ -19,22 +26,25 @@ public:
 public:
     void start_engine()
     {
-        cout << "Start The engine" << name << modle << endl;
+        cout << "Start The engine " << name << modle << endl;
     }
 
     void stop_engine()
     {
-        cout << "Stop The engine" << name << modle << endl;
+        cout << "Stop The engine " << name << modle << endl;
+    }
+    ~Vehicles(){
+        cout << "Im inside Vehicle" << endl;
     }
 };
 
-class Car : public Vehicals
+class Car : public Vehicles
 {
-public:
+protected:
     int noOfdoors;
     string transmission;
-
-    Car(string _name, string _modle, int _noOfTyres, int _noOfdoors, string _transmission) : Vehicals(_name, _modle, _noOfTyres)
+ public:
+    Car(string _name, string _modle, int _noOfTyres, int _noOfdoors, string _transmission) : Vehicles(_name, _modle, _noOfTyres)
     {
         cout << "Im Inside Car ctor" << endl;
         this->noOfdoors = _noOfdoors;
@@ -45,11 +55,35 @@ public:
     {
         cout << "Ac has been strted of" << name << endl;
     }
+    ~Car(){
+     cout << "Im inside Car dtor" << endl;
+    }
+};
+
+class Motercycle: public Vehicles{
+   protected:
+        string handelbarStyle;
+        string Suspenstion;
+public:
+        Motercycle(string _name, string _modle, int _noOfTyres,string _handelbarStyle, string _Suspenstion):Vehicles(_name,_modle,_noOfTyres){
+        this-> handelbarStyle = _handelbarStyle;
+        this -> Suspenstion = _Suspenstion;
+    }
+    void wheelie(){
+        cout << "Whellie hahha" << name << endl;
+    }
+
+    ~Motercycle(){
+        cout << "Im inside Motercyscle dtor" << endl;
+    }
 };
 int main()
 {
     Car A("Maruti800", "LXI", 4, 4, "manual");
     A.start_engine();
     A.startAC();
-        return 0;
+    // A.name()
+    Motercycle m("BMW","VXI",2,"U", "Hard");
+
+    return 0;
 }
