80639fe3df963a91d2197b975c97d2d7c1ee6b11
imaakarsh <aakarsh027@outlook.com>
2026-08-12 00:07:26 +0530

    vector

--- Code/STL/vector.cpp
+++ Code/STL/vector.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
-#include <vector>
+#include <vector> // to include header files to use
 using namespace std;
 
-int main() {
-    
+int main()
+{
+
     // creation
     vector<int> marks;
-     
-    vector<int> miles(20);
+    // cout << *(marks.begin());   // pointing 1st element of vector
+    // vector<int> miles(20);
 
-    vector<int> miles(14,23);
+    // cout << marks.max_size() << endl;
+    marks.push_back(3);
+    marks.push_back(4);
+    marks.push_back(5);
+    marks.push_back(5);
+    marks.push_back(5);
 
-    
+    marks.insert(marks.begin(), 50);
+
+    cout << *marks.begin() << endl;
+
+    // cout << marks.capacity() << endl; // 8
+    // cout << marks.size() << endl;     // 5
+
+    // 5 ll be remove
+    // marks.pop_back();
+
+    // cout << "Size: " << marks.size() << endl;
+
+    // vector<int> miles(14, 23);
+
+    // cout << marks.front() << endl;
+
+    // cout << marks.back() << endl;
 
     return 0;
 }
