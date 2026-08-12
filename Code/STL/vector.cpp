6940f1e0d5eac81bce70c9b192ab8ce75239b844
imaakarsh <aakarsh027@outlook.com>
2026-08-12 21:23:15 +0530

    question

--- Code/STL/vector.cpp
+++ Code/STL/vector.cpp
@@ -5,21 +5,45 @@ using namespace std;
 int main()
 {
 
+    // 2d arry / vector
+    vector<vector<int>> arr(5,vector<int>(4,0));
+    int  totalRows = arr.size();
+
+    // cout << totalRows << endl;
+    // 2d arry created
+    // with 5 row
+
+    // 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // creation
-    vector<int> marks;
+    // vector<int> marks;
     // cout << *(marks.begin());   // pointing 1st element of vector
     // vector<int> miles(20);
 
     // cout << marks.max_size() << endl;
-    marks.push_back(3);
-    marks.push_back(4);
-    marks.push_back(5);
-    marks.push_back(5);
-    marks.push_back(5);
+    // marks.push_back(3);
+    // marks.push_back(4);
+    // marks.push_back(5);
+    // marks.push_back(5);
+    // marks.push_back(5);
 
-    marks.insert(marks.begin(), 50);
+    // marks.insert(marks.begin(), 50);
 
-    cout << *marks.begin() << endl;
+    // cout << *marks.begin() << endl;
 
     // cout << marks.capacity() << endl; // 8
     // cout << marks.size() << endl;     // 5
---
+++ problems/questions/206. Print Counting from 1 to Nc.cpp
@@ -0,0 +1,11 @@
+
+class Solution {
+public:
+    void printCounting(int n) {
+        // Implement the loop logic to print numbers from 1 to n
+        for (int i=1;i<=n;i++){
+            return i;
+        }
+        
+    }
+};
