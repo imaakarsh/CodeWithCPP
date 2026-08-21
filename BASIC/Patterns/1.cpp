18b0e9ff095509cd71ed025a8b95dc3bbf9d57af
rajaakarsh <aakarsh027@outlook.com>
2026-08-21 22:17:27 +0530

    pattern

---
+++ BASIC/Patterns/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void printPattern(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}   
+  
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    printPattern(n);
+}
